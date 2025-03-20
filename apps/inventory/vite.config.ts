978c2d94e87c5454e717f7e0904d674828108a13
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-03-20 13:58:59 -0600

    changes

--- apps/inventory/vite.config.ts
+++ apps/inventory/vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
+const base = process.env.BASE_URL || '/'
+
 export default defineConfig({
-  base: process.env.BASE_URL || '/',
+  base: base,
   plugins: [react()],
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.webp', '**/*.gif']
 })
