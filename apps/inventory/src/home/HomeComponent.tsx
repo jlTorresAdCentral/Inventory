378870919f5047e259574ede544dc93c7ffbbda1
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-03-21 17:31:31 -0600

    use translations

--- apps/inventory/src/home/HomeComponent.tsx
+++ apps/inventory/src/home/HomeComponent.tsx
@@ -1,12 +1,26 @@
+import { useTranslation } from "react-i18next";
 import { screenContainer } from "../assets/styles/core.css";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/Navbar";
+import { useEffect } from "react";
 
 const HomeComponent = () => {
+    const { t, i18n } = useTranslation()
+
+    useEffect(() => {
+        const savedLang = localStorage.getItem("language") || "en";
+        i18n.changeLanguage(savedLang);
+    }, [i18n]);
+
     return (
         <div className={screenContainer}>
             <Navbar />
-            <h1>Home Component</h1>
+            <h1>{t("hello")}</h1>
+            <div>
+                <button onClick={() => i18n.changeLanguage("es")}>🇪🇸 Español</button>
+                <button onClick={() => i18n.changeLanguage("en")}>🇺🇸 English</button>
+                <button onClick={() => i18n.changeLanguage("it")}>🇮🇹 Italiano</button>
+            </div>
             <Footer />
         </div>
     )
