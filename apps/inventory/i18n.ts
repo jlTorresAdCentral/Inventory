6b3d5c3d4e23efdfbbf15f2d9c307a50306c1bc3
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-03-21 18:00:45 -0600

    run build

--- apps/inventory/i18n.ts
+++ apps/inventory/i18n.ts
@@ -1,16 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./locales/en.json";
-import es from "./locales/es.json";
-import it from "./locales/it.json";
 
 i18n
     .use(initReactI18next)
     .init({
         resources: {
             en: { translation: en },
-            es: { translation: es },
-            it: { translation: it },
         },
         lng: "en",
         fallbackLng: "en",
