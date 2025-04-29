4de49e07c03d158263f4f1c8dadbbb62bc63420f
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-04-29 11:09:35 -0600

    buttons styles

--- apps/inventory/src/assets/styles/buttons.css.ts
+++ apps/inventory/src/assets/styles/buttons.css.ts
@@ -23,3 +23,69 @@ export const tableButton = css({
         }
     }
 })
+
+export const whiteButton = css({
+    borderRadius: "12px",
+    textAlign: "center",
+    alignContent: "center",
+    height: "40px",
+    background: "var(--adcentral-white-color)",
+    padding: "4px 10px",
+    boxSizing: "border-box",
+    color: "var(--adcentral-purple)",
+    md: {
+        cursor: "pointer",
+        _hover: {
+            background: "var(--adcentral-purple)",
+            color: "var(--adcentral-white-color)",
+        }
+    },
+})
+
+export const cancelFormButton = css({
+    borderRadius: "8px",
+    maxWidth: "300px",
+    padding: "5px 20px",
+    boxSizing: "border-box",
+    color: "var(--adcentral-black-color)",
+    border: "1px solid var(--adcentral-light-gray)",
+    md: {
+        cursor: "pointer",
+        _hover: {
+            background: "var(--adcentral-light-gray)",
+            border: "1px solid var(--adcentral-purple)",
+            "& label": {
+                cursor: "inherit",
+            }
+        }
+    }
+})
+
+export const saveFormButton = css({
+    borderRadius: "8px",
+    maxWidth: "300px",
+    padding: "5px 20px",
+    boxSizing: "border-box",
+    color: "var(--adcentral-black-color)",
+    border: "1px solid var(--adcentral-light-gray)",
+    _disabled: {
+        cursor: "initial",
+        color: "var(--adcentral-gray-color)",
+        _hover: {
+            background: "initial",
+            border: "1px solid var(--adcentral-light-gray)",
+            color: "var(--adcentral-gray-color)",
+        }
+    },
+    md: {
+        cursor: "pointer",
+        _hover: {
+            background: "var(--adcentral-purple)",
+            border: "1px solid var(--adcentral-white-color)",
+            color: "var(--adcentral-white-color)",
+            "& label": {
+                cursor: "inherit",
+            }
+        }
+    }
+})
