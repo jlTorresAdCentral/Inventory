e05c0f58e980fb18199aaf9ea344983642b1ca50
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-04-29 13:31:50 -0600

    routes names

--- apps/inventory/src/navigation/navigation-routes-names.ts
+++ apps/inventory/src/navigation/navigation-routes-names.ts
@@ -2,5 +2,6 @@ export const HOME_PATH = "/inventory"
 export const DOS_PATH = "/Inventory"
 export const RESPONSIVE_PATH = "/responsive"
 export const ABOUT_PATH = "/about"
+export const LOGIN = HOME_PATH + "/login"
 
 export const INVENTORY_CREATE = HOME_PATH + "/create"
