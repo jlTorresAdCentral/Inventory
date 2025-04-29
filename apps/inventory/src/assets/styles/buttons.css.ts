e089271ac7fc6a0bf2c58b65e6b67c69bf39eb19
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-04-29 15:28:42 -0600

    just change

--- apps/inventory/src/assets/styles/buttons.css.ts
+++ apps/inventory/src/assets/styles/buttons.css.ts
@@ -16,7 +16,6 @@ export const tableButton = css({
     color: "var(--adcentral-white-color)",
     background: "var(--adcentral-purple)",
     minWidth: "250px",
-    cursor: "initial",
     md: {
         cursor: "pointer",
         _hover: {
