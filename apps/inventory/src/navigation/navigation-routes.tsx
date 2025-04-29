b52051439c2a5bc9b83015e58397a2377abfc774
jlTorresAdCentral <80363522+jlTorresAdCentral@users.noreply.github.com>
2025-04-29 13:31:02 -0600

    routes

--- apps/inventory/src/navigation/navigation-routes.tsx
+++ apps/inventory/src/navigation/navigation-routes.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router";
-import { HOME_PATH, INVENTORY_CREATE } from "./navigation-routes-names";
+import { HOME_PATH, INVENTORY_CREATE, LOGIN } from "./navigation-routes-names";
 import HomeComponent from "../home/HomeComponent";
 import CreateInventoryScreen from "../home/CreateInventoryScreen";
+import LoginScreen from "../login/login-screen";
 const NavigationRoutes = () => {
     return (
         <Routes>
             <Route
                 path={HOME_PATH}
                 element={<HomeComponent />}
+            />
+            <Route
+                path={LOGIN}
+                element={<LoginScreen />}
             />
             <Route
                 path={INVENTORY_CREATE}
